197fbe7fc90f9fd88b9ac75803b0cea4ef32dba4
stacievans <1586953993@qq.com>
2026-08-19 10:49:55 +0800

    更新版本及说明

--- src/release.js
+++ src/release.js
@@ -1,7 +1,7 @@
 const release = {
-  version: '0.1.0',
-  date: '2026-08-14',
-  summary: '绑定校验交互调整为点击后查询提示；新增版本信息展示',
+  version: '0.1.1',
+  date: '2026-08-19',
+  summary: '标注工作台驳回时问题标签必填',
 }
 
 export default release
